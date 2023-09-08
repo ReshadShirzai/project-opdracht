130f3b357b20a5bca550c146a3fc7b9f7efd763b
kinan232 <144129145+kinan232@users.noreply.github.com>
2023-09-08 09:45:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
-function getrandomnumber() {
-    return Math.floor(Math.random() * 6) + 1;
-}
+let computerScore = 0;
+let playerScore = 0;
 
-const randomNumber = getrandomnumber();
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+ }
+
+function getRandomNumber() {
+    return getRandomIntInclusive(1, 6);
+}
 
 const roll = document.querySelector('.roll');
 roll.addEventListener('click', function() {
-    console.log(randomNumber);
+    console.log(getRandomNumber());
 });
+
+
+let diceOne = getRandomNumber();
+let diceTwo = getRandomNumber();
+let total = diceOne + diceTwo;
+
+console.log('Dobbelsteen 1: ', diceOne);
+console.log('Dobbelsteen 2: ', diceTwo);
+console.log('Gegooide dobbelstenen: ', total);
+
+
+setTimeout(function(){
+    let diceThree = getRandomNumber();
+    let diceFour = getRandomNumber();
+    total = diceThree + diceFour;
+
+    console.log('Dobbelsteen 3: ', diceThree);
+    console.log('Dobbelsteen 4: ', diceFour);
+    console.log('Gegooide dobbelstenen computer: ', total);
+}, 3000);
