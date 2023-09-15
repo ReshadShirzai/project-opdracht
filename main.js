99614d33fecd994594a67edeb23783b659887f9e
Arsenio-W <302347625@student.rocmondriaan.nl>
2023-09-15 10:18:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 let computerScore = 0;
 let playerScore = 0;
 
+let randomNumber1 = 0;
+let randomNumber2 = 0;
 
 const roll = document.querySelector('.roll');
 roll.addEventListener('click', function() {
-    const randomNumber1 = getRandomNumber();
-    const randomNumber2 = getRandomNumber();
+    randomNumber1 = getRandomNumber();
+    randomNumber2 = getRandomNumber();
     console.log(randomNumber1 + " " + randomNumber2); // Voeg een spatie tussen de getallen toe
     const dobbel1 =document.querySelector(".dobbel1")
     const dobbel2 =document.querySelector(".dobbel2")
     dobbel1.src = "img/dobbel" +randomNumber1+ ".png" 
-    dobbel2.src = "img/dobbel" +randomNumber2+ ".png" 
+    dobbel2.src = "img/dobbel" +randomNumber2+ ".png"
 }); 
 console.log('hallo')
 
@@ -23,24 +25,45 @@ function getRandomIntInclusive(min, max) {
 function getRandomNumber() {
     return getRandomIntInclusive(1, 6);
 }
+let randomNumber3 = getRandomNumber();
+let randomNumber4 = getRandomNumber();
 
-let diceOne = getRandomNumber();
-let diceTwo = getRandomNumber();
-let total = diceOne + diceTwo;
+let totalPlayer = 0;
+let totalComputer = 0;
 
-console.log('Dobbelsteen 1: ', diceOne);
-console.log('Dobbelsteen 2: ', diceTwo);
-console.log('Gegooide dobbelstenen: ', total);
+const btnHigher = document.querySelector('.Hoger');
+btnHigher.addEventListener('click', function() {
+    totalPlayer = randomNumber1 + randomNumber2;
+    totalComputer = randomNumber3 + randomNumber4;
+if (totalPlayer > totalComputer){
+    console.log("jij wint!")
+} else if(totalPlayer < totalComputer) {
+    console.log("Computer wint!")
+} else {
+    console.log("Gelijkspel!")
+}
+});
+
+const BtnLower = document.querySelector('.Lager');
+BtnLower.addEventListener('click', function() {
+    totalPlayer = randomNumber1 + randomNumber2;
+    totalComputer = randomNumber3 + randomNumber4;
+if (totalPlayer < totalComputer){
+    console.log("jij wint!")
+} else if(totalPlayer > totalComputer) {
+    console.log("Computer wint!")
+} else {
+    console.log("Gelijkspel!")
+}
+}); 
+
+
 
 
 setTimeout(function(){
-    let diceThree = getRandomNumber();
-    let diceFour = getRandomNumber();
-    total = diceThree + diceFour;
-
-    console.log('Dobbelsteen 3: ', diceThree);
-    console.log('Dobbelsteen 4: ', diceFour);
-    console.log('Gegooide dobbelstenen computer: ', total);
+    console.log('Dobbelsteen 3: ', randomNumber3);
+    console.log('Dobbelsteen 4: ', randomNumber4);
+    console.log('Gegooide dobbelstenen computer: ', totalComputer);
 }, 3000);
 
 //r.i.p Pascal
